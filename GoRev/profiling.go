0c406f566617556a8ac639b4514184f8a5fd04dc
Riccardo <134144784+riccio8@users.noreply.github.com>
2024-05-16 22:01:10 +0200

    Update profiling.go

--- GoRev/profiling.go
+++ GoRev/profiling.go
@@ -1,1 +1,43 @@
+// this provide a simple overview about CPU usage
 
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"runtime/pprof"
+	"time"
+)
+
+func cpu(start time.Time, done chan struct{}) {
+	f, err := os.Create("cpu_profile.prof")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
+
+	for i := 0; i < 10000; i++ {
+		time.Sleep(time.Millisecond)
+	}
+
+	// this part informs the "done" channel that CPU profiling is completed 
+	done <- struct{}{}
+}
+
+func main() {
+	start := time.Now()
+
+	done := make(chan struct{})
+
+	go cpu(start, done)
+
+	<-done
+
+	elapsed := time.Since(start)
+	fmt.Printf("page took %s\n", elapsed)
+}
+// to finish this script
